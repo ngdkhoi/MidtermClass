af4b5c58158eb1c711be41bf4e85203d8cae9f0d
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-11-07 17:53:56 +0700

    sign in and sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import Signup from "./components/authentication/registerForm";
+import Login from "./components/authentication/loginForm";
+import { Routes, Route, Link, Redirect, useRoutes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  const element = useRoutes([
+    {
+      path: '/',
+      element: <Login />
+    },
+    {
+      path: '/register',
+      element: <Signup />
+    }
+  ])
+
+  return element
 }
 
 export default App;
